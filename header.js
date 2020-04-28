898509147e9ff3a15cac2ebc43e2b92829baeb9d
justaweb.dev <3829508-justaweb.dev@users.noreply.gitlab.com>
2020-04-28 14:44:29 +0200

    fixed extending

--- header.js
+++ header.js
@@ -1,7 +1,10 @@
-export const header = ({ module }) => module.text.bold.extend(({ subs: { counter }, self, store }) => self.css`
-font-weight: bold;
+export const header = ({ module }) =>
+  module.text.bold.extend(({ subs: { counter }, self, store }) =>
+    self.css`
 display: block;
 background: blue;
 width: 50%;
-`.nest('Header')
-    .on({ click: () => store.counter.next(counter + 1) }))
+`
+      .nest("Header")
+      .on({ click: () => store.counter.next(counter + 1) })
+  );
--- text.js
+++ text.js
@@ -2,8 +2,11 @@ export const normal = ({ self }) => self.css`
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
     font-weight: 300;
     font-size: 20px;
-`
+`;
 
-export const bold = ({ module }) => module.text.normal.extend(({ self }) => self.css`
+export const bold = ({ module }) =>
+  module.text.normal.extend(
+    ({ self }) => self.css`
     font-weight: 500;
-`)
+`
+  );
