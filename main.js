e38f3f10caeb90e828cb4294e2404ffb9aa65150
nuuh5 <izgetrekt@gmail.com>
2020-05-06 17:11:04 +0200

    bugfix

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ import { Remarkable } from "https://unpkg.com/remarkable@2.0.0/dist/esm/index.br
 
 const md = new Remarkable({ html: true });
 
+const getBaseURL = () =>
+  window.location.host === "nuuh5.github.io"
+    ? "https://cdn.jsdelivr.net/gh/nuuh5/SchulProjektWeb/pages/"
+    : "./pages/";
+
 export const body = ({ subs: { route }, self }) => {
   self.css`display: block;
     max-width: 100%;
@@ -11,7 +16,7 @@ export const body = ({ subs: { route }, self }) => {
     margin: 20px auto;
     `;
 
-  fetch(`./pages/${Object.values(routes)[route] || "index"}.md`)
+  fetch(`${getBaseURL()}${Object.values(routes)[route] || "index"}.md`)
     .then((response) => response.text())
     .then((text) => {
       self.innerHTML = md.render(text.split("---")[2]);
