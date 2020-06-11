dfc73438ebe701af022dad587a391f8ef0263ff5
justaweb.dev <3829508-justaweb.dev@users.noreply.gitlab.com>
2020-06-11 19:15:22 +0200

    fixed bg

--- entry.js
+++ entry.js
@@ -1,18 +1,19 @@
 const background = new Image();
 
-background.src = 'https://images.unsplash.com/photo-1440581572325-0bea30075d9d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80';
+background.src =
+  "https://images.unsplash.com/photo-1440581572325-0bea30075d9d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80";
 background.style.position = "fixed";
 background.style.display = "block";
 background.style.top = "-10px";
 background.style.left = "-10px";
 background.style.right = "-10px";
 background.style.bottom = "-10px";
-background.style.minWidth = "calc(100vw + 20px)"
-background.style.zIndex = '-1';
-background.style.filter = 'blur(10px)';
+background.style.minWidth = "calc(100vw + 20px)";
+background.style.minHeight = "calc(100vh + 20px)";
+background.style.zIndex = "-1";
+background.style.filter = "blur(10px)";
 
-document.body.style.background = "black"
-
+document.body.style.background = "black";
 
 export const main = ({ module }) =>
   module.text.normal.extend(
